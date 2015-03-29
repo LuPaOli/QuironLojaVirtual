be83fc14db58c6380804282451cb13b6f45e5a8e
Lucas de Paula Oliveira <lucas_oliveira_17@hotmail.com>
2015-03-29 18:23:13 -0300

    Alterações em carrinho

---
+++ Quiron.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Entidades;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+
+        private ProdutosRepositorio _repositorio;
+
+        public RedirectToRouteResult Adicionar(int produtoId, string returnUrl)
+        {
+            //Criação do repositório de dados
+            _repositorio = new ProdutosRepositorio();
+
+            //Recupera primeiro produto do repositório e verifica se ele existe...
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            //Verifica se o produto a ser verificado não é nulo...
+            if (produto != null)
+            {
+                ObterCarrinho().AdicionarItem(produto,1);
+            }
+            return RedirectToAction("Index", new {returnUrl});
+
+        }
+
+        private Carrinho ObterCarrinho()
+        {
+            //A classe de obter carrinhos é uma sessão, onde se armazenará os itens dos carrinhos...
+            Carrinho carrinho = (Carrinho) Session["Carrinho"];
+
+            if (carrinho != null)
+            {
+                carrinho = new Carrinho();
+                Session["Carrinho"] = carrinho;
+            }
+
+            return carrinho;
+        }
+
+        public RedirectToRouteResult Remover(int produtoId, string returnUrl)
+        {
+            //Criação do repositório de dados
+            _repositorio = new ProdutosRepositorio();
+
+            //Recupera primeiro produto do repositório e verifica se ele existe...
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            //Verifica se o produto a ser verificado não é nulo...
+            if (produto != null)
+            {
+                ObterCarrinho().RemoverItem(produto);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+
+        }
+    }
+}
